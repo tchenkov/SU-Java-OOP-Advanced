30dc0c08a7b3cf48f88a85fdcfec28c9e153d94c
Todor Chenkov <joto_to@mail.bg>
2017-11-29 00:12:31 +0200

    update L07_EnumerationsAndAnnotations_Lab\P05_CodingTracker\Main.java v2

--- src/L07_EnumerationsAndAnnotations_Lab/P05_CodingTracker/Main.java
+++ src/L07_EnumerationsAndAnnotations_Lab/P05_CodingTracker/Main.java
@@ -2,6 +2,6 @@ package L07_EnumerationsAndAnnotations_Lab.P05_CodingTracker;
 
 public class Main {
     public static void main(String[] args) {
-        Tracker.main(new String[]{});
+        Tracker.main(null);
     }
 }
